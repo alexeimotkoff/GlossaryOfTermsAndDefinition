78df6d83f2e580c52ff47473fd6b725ea9ad86a4
alexeimotkoff <alexeimotkoff@gmail.com>
2017-05-15 20:41:23 +0600

    Добавлено начало реализации контроллеров и частичных представлений

--- TermsAndDefinitions.WebUI/Controllers/EditTermController.cs
+++ TermsAndDefinitions.WebUI/Controllers/EditTermController.cs
@@ -4,21 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TermsAndDefinitions.WebUI.Models;
-using TermsAndDefinitions.WebUI.ViewModels;
 
 namespace TermsAndDefinitions.WebUI.Controllers
 {
-    public class HomeController : Controller
+    public class EditTermController : Controller
     {
         //
-        // GET: /Home/
+        // GET: /EditTerm/
         projects_dataBaseEntities2 db = new projects_dataBaseEntities2();
-
         public ActionResult Index()
         {
-            var terms = db.Terms.Select((x) => new VTerm(x));
-            
-            return View(terms);
+            return View();
         }
 
     }
---
+++ TermsAndDefinitions.WebUI/ViewModels/SearchModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TermsAndDefinitions.WebUI.ViewModels
+{
+    public class SearchQuery
+    {
+      public  SearchQuery( int? type, string query = "", int  count=10)
+        {
+            countSearchItem = count;
+            querySearch = query;
+            typeSearching = type;
+        }
+
+     
+      public string querySearch { get; set; }
+      public  int countSearchItem {get; set;}
+       public int? typeSearching { get; set; }
+    }
+}
