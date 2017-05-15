b5e075c7aa1cd36dbec68ea9d3a9c3304d396baa
alexeimotkoff <alexeimotkoff@gmail.com>
2017-05-15 16:58:55 +0600

    Добалвено представление; Добвлены конструкторы  объекты моделей

--- TermsAndDefinitions.WebUI/ViewModels/ViewModels.cs
+++ TermsAndDefinitions.WebUI/ViewModels/ViewModels.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TermsAndDefinitions.WebUI.Models;
 
 namespace TermsAndDefinitions.WebUI.ViewModel
 {
@@ -11,8 +12,16 @@ namespace TermsAndDefinitions.WebUI.ViewModel
         {
             TermName = termName;
         }
+
+        public VTerm(Term term)
+        {
+            TermName = term.TermName;
+            Definition discription = term.Definitions.OrderByDescending(y => y.Frequency).FirstOrDefault();
+            Description = new VDefinition(discription);
+        }
+
         public string TermName { get; set; }
-        public VDefinition Definition { get; set; }
+        public VDefinition Description { get; set; }
     }
 
     public class VDefinition
@@ -21,6 +30,12 @@ namespace TermsAndDefinitions.WebUI.ViewModel
         {
             Description = description;
             URL = url;
+        }
+
+        public VDefinition( Definition  definition)
+        {
+            Description = definition.Description;
+            URL = definition.URL;
         }
        public string Description { get; set; }
        public string URL { get; set; }
