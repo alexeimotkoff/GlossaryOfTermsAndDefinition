8b31c182d8340aca054ce9a6a07c4080528836c0
alexeimotkoff <alexeimotkoff@gmail.com>
2017-05-15 14:17:57 +0600

    изменение

--- TermsAndDefinitions.WebUI/Models/GlossaryProjectDB_Model.Designer.cs
+++ TermsAndDefinitions.WebUI/Models/GlossaryProjectDB_Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Vuvu\Documents\GitHub\GlossaryOfTermsAndDefinition\TermsAndDefinitions.WebUI\Models\GlossaryProjectDB_Model.edmx" включено. 
+﻿// Создание кода T4 для модели "B:\Универ\ВКР\Термины и определения\TermsAndDefinitions\TermsAndDefinitions.WebUI\Models\GlossaryProjectDB_Model.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
