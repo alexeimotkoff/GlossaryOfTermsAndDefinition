6fd6ce3e4fba3f9ec7e78cd7fd8d181efe29ce2b
Vladimir Savkin <just.a.man@list.ru>
2017-05-17 02:07:30 +0600

    Добавлен костыльный поиск по алфавиту

--- TermsAndDefinitions.WebUI/Controllers/SearchController.cs
+++ TermsAndDefinitions.WebUI/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using TermsAndDefinitions.WebUI.Models;
@@ -26,38 +27,60 @@ namespace TermsAndDefinitions.WebUI.Controllers
 
         public ActionResult GetTerms(SearchQuery searchQuery)
         {
-            IQueryable<Term> searchResult = db.Terms.Where(x => x.TermName.Contains(searchQuery.querySearch)).OrderBy(x => x.TermName);
+           
+            IQueryable<Term> searchResult = db.Terms.OrderBy(x => x.TermName);
             int takeCount = Math.Min(searchQuery.countSearchItem, searchResult.Count());
+            
+            if (!string.IsNullOrEmpty(searchQuery.querySearch))
+            {
+                if(searchQuery.IsChar)
+                    searchResult = searchResult.Where(x => x.TermName.ToLower()[0] == searchQuery.querySearch[0]);
+                else
+                    searchResult = searchResult.Where(x => x.TermName.ToLower().Contains(searchQuery.querySearch));
+            }
             if (takeCount > 0)
                 searchResult = searchResult.Take(takeCount);
-
            List<VTerm> searchResultToViewModel = new List<VTerm>();
-            foreach (var term in searchResult)
+            foreach (var term in searchResult.ToList())
                 searchResultToViewModel.Add(new VTerm(term));            
             return View(searchResultToViewModel);
         }
 
         public ActionResult GetTermsByDefinition(SearchQuery searchQuery)
         {
-            IQueryable<Definition> searchResult = db.Definitions.Where(x => x.Description.Contains(searchQuery.querySearch)).OrderBy(x => x.Term.TermName);
+            IQueryable<Definition> searchResult = db.Definitions.OrderBy(x => x.Term.TermName);
             int takeCount = Math.Min(searchQuery.countSearchItem, searchResult.Count());
+            if (!string.IsNullOrEmpty(searchQuery.querySearch))
+            {
+                if (searchQuery.IsChar)
+                    searchResult = searchResult.Where(x => x.Description.ToLower()[0] == searchQuery.querySearch[0]);
+                else
+                    searchResult = searchResult.Where(x => x.Description.ToLower().Contains(searchQuery.querySearch));
+            }
             if (takeCount > 0)
                 searchResult = searchResult.Take(takeCount);
             List<VTerm> searchResultToViewModel = new List<VTerm>();
-            foreach (var item in searchResult)
+            foreach (var item in searchResult.ToList())
                 searchResultToViewModel.Add(new VTerm(item.Term.TermName) { Description = new VDefinition(item)});
             return View(searchResultToViewModel);
         }
 
         public ActionResult GetProjects(SearchQuery searchQuery)
         {
-            IQueryable<Project> searchResult = db.Projects.Where(x => x.ProjectName.Contains(searchQuery.querySearch)).OrderBy(x => x.ProjectName);
+            IQueryable<Project> searchResult = db.Projects.OrderBy(x => x.ProjectName);
             int takeCount = Math.Min(searchQuery.countSearchItem, searchResult.Count());
+            if (!string.IsNullOrEmpty(searchQuery.querySearch))
+            {
+                if (searchQuery.IsChar)
+                    searchResult = searchResult.Where(x => x.ProjectName[0] == searchQuery.querySearch[0]);
+                else
+                    searchResult = searchResult.Where(x => x.ProjectName.Contains(searchQuery.querySearch));
+            }
             if (takeCount > 0)
                 searchResult = searchResult.Take(takeCount);
             List<VProject> searchResultToViewModel = new List<VProject>();
-            foreach (var project in searchResult) searchResultToViewModel.Add(new VProject(project));
-            
+            foreach (var project in searchResult.ToList())
+                searchResultToViewModel.Add(new VProject(project));            
             return View(searchResultToViewModel);
         }
     }
--- TermsAndDefinitions.WebUI/ViewModels/SearchModels.cs
+++ TermsAndDefinitions.WebUI/ViewModels/SearchModels.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace TermsAndDefinitions.WebUI.ViewModels
 {
     public class SearchQuery
     {
+        Regex rx = new Regex("[а-яёa-z]");
+        
         public SearchQuery() {
             countSearchItem = 10;
             querySearch = "";
@@ -17,11 +20,17 @@ namespace TermsAndDefinitions.WebUI.ViewModels
         public  SearchQuery(string query = "", int  count=10, int type = -1)
         {
             countSearchItem = count;
-            querySearch = query;
+            querySearch = query.ToLower();
             typeSearching = type;
         }
 
-     
+      public bool IsChar
+        {
+            get
+            {
+                return rx.Match(querySearch).Success && querySearch.Length == 1;
+            }
+        }
       public string querySearch { get; set; }
       public  int countSearchItem {get; set;}
       public int typeSearching { get; set; }
