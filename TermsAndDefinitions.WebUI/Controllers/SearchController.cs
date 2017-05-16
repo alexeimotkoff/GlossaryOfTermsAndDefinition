5c3692690e0bee306e3b49da5fdc0e4097ce079d
Vladimir Savkin <just.a.man@list.ru>
2017-05-16 20:48:29 +0600

    Измен Search.cshtml

--- TermsAndDefinitions.WebUI/Controllers/SearchController.cs
+++ TermsAndDefinitions.WebUI/Controllers/SearchController.cs
@@ -16,14 +16,14 @@ namespace TermsAndDefinitions.WebUI.Controllers
         GlossaryProjectDatabaseEntities db = new GlossaryProjectDatabaseEntities();
         public ActionResult Index()
         {
-            SearchQuery searchQuery = new SearchQuery(null);
-            return View();
-        }
-        
-        public ActionResult Search(SearchQuery searchQuery)
-        {
+            SearchQuery searchQuery = new SearchQuery("");
             return View(searchQuery);
         }
+        
+        //public ActionResult Search(SearchQuery searchQuery)
+        //{
+        //    return View(searchQuery);
+        //}
 
         public ActionResult GetTerms(SearchQuery searchQuery)
         {
@@ -51,17 +51,17 @@ namespace TermsAndDefinitions.WebUI.Controllers
             return View(searchResult);
         }
 
-        public ActionResult GetTermsByProject(SearchQuery searchQuery)
-        {
-            IEnumerable<VTerm> searchResult;
-            if (searchQuery.countSearchItem > 0)
-                searchResult = db.Projects.Where(x => x.ProjectName.Contains(searchQuery.querySearch)).Intersect(db.Terms.Where(x => x.Projects)).;
-            else
-                searchResult = db.Definitions
-                   .Where(x => x.Description.Contains(searchQuery.querySearch)).OrderBy(x => x.Term.TermName)
-                   .Select(x => new VTerm(x.Term.TermName) { Description = new VDefinition(x) });
+        //public ActionResult GetTermsByProject(SearchQuery searchQuery)
+        //{
+        //    //IEnumerable<VTerm> searchResult;
+        //    //if (searchQuery.countSearchItem > 0)
+        //    //    searchResult = db.Projects.Where(x => x.ProjectName.Contains(searchQuery.querySearch)).Intersect(db.Terms.Where(x => x.Projects)).;
+        //    //else
+        //    //    searchResult = db.Definitions
+        //    //       .Where(x => x.Description.Contains(searchQuery.querySearch)).OrderBy(x => x.Term.TermName)
+        //    //       .Select(x => new VTerm(x.Term.TermName) { Description = new VDefinition(x) });
 
-            return View(searchResult);
-        }
+        //    //return View(searchResult);
+        //}
     }
 }
